7acbface308e95e8e3fcd5f58c4bbbda938a13a4
SabaVirk <saba.github.22@gmail.com>
2022-01-05 00:12:07 -0600

    changed in eclipse

--- src/Git.java
+++ src/Git.java
@@ -6,9 +6,9 @@ public static void main(String[] args) {
 	
 }
 	public void method() {
-	System.out.println("These are changes in upstream, upstream means Remote Depository")
+	System.out.println("These are changes in upstream, upstream means Remote Depository");
 		}
-	public void method() {
+	public void methodA() {
 		//This is second method for practice purpose
 	
 }
