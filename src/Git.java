ae922c553b9ac3c226084104be31e5fe517f08a7
SabaVirk <97071794+SabaVirk@users.noreply.github.com>
2022-01-04 18:52:48 -0600

    Update Git.java
    This is for practice purpose

--- src/Git.java
+++ src/Git.java
@@ -2,5 +2,11 @@
 public class Git {
 public static void main(String[] args) {
 	
+	public void method() {
+	System.out.println("These are changes in upstream, upstream means Remote Depository")
+		}
+	public void method() {
+		//This is second method for practice purpose
+	
 }
 }
